c05a1fe669903f95d2f7014d65a73d293e2dba61
Monica Chang <MonicaChang@Monicas-MacBook-Pro.local>
2016-07-17 18:38:05 -0400

    bouncing apple

--- applesandbananas.js
+++ applesandbananas.js
@@ -10,13 +10,16 @@ $(document).ready(function() {
 	});
 
 
-
-
 	$("#verse1").hover(function() {
 	    $(this).animate({color:"#a90000"}, 1000);
 	}, function() {
 	    $(this).animate({color:"white"}, 1000);
 	});
+
+
+
+	
+
 
 	$("#verse2").hover(function() {
 	    $(this).animate({color:"#ad0000"}, 1000);
@@ -55,21 +58,38 @@ $(document).ready(function() {
 			alert($(this).val());
 		});
 
+	$("#verse1apple").click(function() {
+		$(this).animate({top: "+=450", left: "+=50"}, 1000);
+		$(this).animate({top: "-=380", left: "+=60"}, 1000);
+		$(this).animate({top: "+=380", left: "+=70"}, 1000);
+		$(this).animate({top: "-=285", left: "+=81"}, 1000);
+		$(this).animate({top: "+=285", left: "+=92"}, 1000);
+		$(this).animate({top: "-=140", left: "+=104"}, 1000);
+		$(this).animate({top: "+=140", left: "+=116"}, 1000);
+		$(this).animate({top: "-=60", left: "+=129"}, 1000);
+		$(this).animate({top: "+=60", left: "+=143"}, 1000); 
+	});
 });
 
 
+function toggleSound1() {
 
-function play(myVowels){
-       var audio = document.getElementById(myVowels);
-       audio.play();
-   }
+  var audioElem = document.getElementById("audio1");
 
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("listenbutton").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("listenbutton").value="LISTEN";
+	}
+}
 
-
-
-function toggleSound() {
+function toggleSoundA() {
 
   var audioElem = document.getElementById("audioA");
+
   if (audioElem.paused) {
     audioElem.play();
 	document.getElementById("buttonA").value="||";
@@ -80,5 +100,72 @@ function toggleSound() {
 	}
 }
 
+function toggleSoundE() {
+
+  var audioElem = document.getElementById("audioE");
+
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonE").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonE").value="E";
+	}
+}
+
+function toggleSoundI() {
+
+  var audioElem = document.getElementById("audioI");
+
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonI").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonI").value="I";
+	}
+}
+
+function toggleSoundO() {
+
+  var audioElem = document.getElementById("audioO");
+
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonO").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonO").value="O";
+	}
+}
+
+function toggleSoundU() {
+
+  var audioElem = document.getElementById("audioU");
+
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonU").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonU").value="U";
+	}
+}
+/*function toggleSound() {
+
+  var audioElem = document.getElementById("audioA");
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonA").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonA").value="A";
+	}
+}*/
 
 
