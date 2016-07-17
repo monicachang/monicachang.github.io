d454600be35941dbdc9bf52f49270793be64beb3
Monica Chang <MonicaChang@Monicas-MBP.fios-router.home>
2016-07-17 02:03:21 -0400

    play and pause working for a button

--- applesandbananas.js
+++ applesandbananas.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 
 
 	$("#learn").hover(function(){
-	$("#learn").animate({color: "black"}, 1200);
+	$("#learnheading").animate({color: "black"}, 1200);
 	},
 	function(){
-		$("#learn").stop().animate({color:"white"});
+		$("#learnheading").stop().animate({color:"white"});
 	});
+
 
 
 
@@ -61,25 +62,23 @@ $(document).ready(function() {
 function play(myVowels){
        var audio = document.getElementById(myVowels);
        audio.play();
+   }
 
-       $("#myVowels").click(function() {
-    if (audio.paused == false) {
-        audio.pause();
-        //alert('music paused');
-    } else {
-        audio.play();
-        //alert('music playing');
-    }
-});
+
+
+
+function toggleSound() {
+
+  var audioElem = document.getElementById("audioA");
+  if (audioElem.paused) {
+    audioElem.play();
+	document.getElementById("buttonA").value="||";
+	}
+  else {
+    audioElem.pause();
+	document.getElementById("buttonA").value="A";
+	}
 }
 
-/*$('.player_audio').click(function() {
-    if ($('.player_audio').paused == false) {
-        $('.player_audio').pause();
-        alert('music paused');
-    } else {
-        $('.player_audio').play();
-        alert('music playing');
-    }
-});*/
+
 
