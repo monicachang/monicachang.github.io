763c38c7a2c00a65a01b1ca6c640a58f5330cf37
Monica Chang <MonicaChang@Monicas-MBP.fios-router.home>
2016-07-21 00:30:18 -0400

    css is good, trouble with javascript

--- chat/chat_client.js
+++ chat/chat_client.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	var socket = io("http://localhost:3000"); //connect to the server or "pizza place"
+	var wordSearch;
 
 	$("#chat-start").click(function(){//when you refresh the page, enter your username, and click the start chatting button, you will get your past conversation
 		$.ajax({
@@ -32,6 +33,33 @@ $(document).ready(function() {
 		socket.emit("chat message", messageObject); //the function is to emit a command called chat message (we get to name this command "chat message") and along with that command attach some information to it (the value)//"chat message is let the server know that it's going to be a message"
 		$("#chat-input").val(""); //empties the box when you hit enter
 		return false; //prevents from refreshing page		
+
+		e.preventDefault();
+				wordSearch = $("#chat-input").val()
+				//console.log($("#chat-input").val());
+
+				$.ajax({
+					url: "https://api.spotify.com/v1/search",
+					data: {
+						q: wordSearch,
+						type: "track",
+						limit: "25"
+					},
+					success: ajaxHandler
+				});
+		});
+
+	function ajaxHandler(data)
+		{
+			console.log(data);
+
+			var results = data.tracks.items; //array of albums items (nested objects)
+			for(var i = 0; i < results.length; i++) //interate through each item in array of albums
+			{
+				$("#api_result").append(results[i].name + "\n"); //change the .name to .id would give you the ids of the albums instead of the names
+			}
+		}
+
 	});
 
 	socket.on('chat message', function(msg){ //when we get an event called chat message, run the following function w/ parameter called msg and append to our list that message
