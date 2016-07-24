52ffd36b24a27dab42d5432e5936469bef3d9782
Monica Chang <MonicaChang@Monicas-MBP.fios-router.home>
2016-07-23 23:28:07 -0400

    color changes and local timestamp working

--- chat/public/chat_client.js
+++ chat/public/chat_client.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
 
 	var socket = io(); //connect to the server or "pizza place"
+
+	var d = new Date();
+				var milliseconds = d.getTime(); //the number of milliseconds since midnight January 1, 1970
+				//console.log(milliseconds);
+				var timezonediffminutes = d.getTimezoneOffset(); // difference between UTC time and local time
+				var seconds = milliseconds / 1000;
+				var minutes = seconds / 60 - timezonediffminutes; //this is where I account for the time zone difference and gives you local time
+				var hours = minutes / 60;
+				var days = hours / 24;
+				var years = days / 365;
+				var hourstoday = Math.floor(hours) - Math.floor(days)*24;
+				var minutestoday = Math.floor(minutes) - Math.floor(hours)*60;
+				
+				if (minutestoday < 10)
+				{
+					minutestoday = "0" + minutestoday;
+				}
+
+				var time;
+				if (hourstoday == 0)
+				{
+					time = "12" + ":" + minutestoday + " am";
+				}else if (hourstoday >= 1 && hourstoday < 12) 
+				{
+					time = hourstoday + ":" + minutestoday + " am";
+				}else if (hourstoday == 12)
+				{
+					time = "12" + ":" + minutestoday + " pm";
+				}else
+				{
+					time = (hourstoday - 12) + ":" + minutestoday + " pm";
+				}
 	
 
 	$("#chat-start").click(function(){//when you refresh the page, enter your username, and click the start chatting button, you will get your past conversation
@@ -13,9 +45,9 @@ $(document).ready(function() {
 				var msg = data[i]; //set msg to each thing in the data
 				if(msg.username == $("#chat-name").val()) //see if username in chat equals name in data field
 				{
-					$("#chat-log ul").append("<li class='me-line'><b>" + msg.username +  "</b>: <i>" + msg.text + "</i></li>"); //use ul because it's in the ul
+					$("#chat-log ul").append("<li class='me-line'><b>" + msg.username +  "</b>: <i>" + msg.text + " (" + time + ")</i></li>"); //use ul because it's in the ul
 				}else{
-					$("#chat-log ul").append("<li class='them-line'><b>" + msg.username +  "</b>: <i>" + msg.text + "</i></li>");
+					$("#chat-log ul").append("<li class='them-line'><b>" + msg.username +  "</b>: <i>" + msg.text + " (" + time + ")</i></li>");
 
 				}	
 			}
@@ -30,11 +62,12 @@ $(document).ready(function() {
 		messageObject.username = $("#chat-name").val();
 		messageObject.text = $("#chat-input").val();
 
-		//e.preventDefault();
 				var messagesplit = messageObject.text.split(" ");
 				var lastword = messagesplit[messagesplit.length - 1]
 				var numberofwords = messagesplit.length;
-				console.log(lastword);
+				var buttoncolorchange = messagesplit.length * 25;
+				//console.log(lastword);
+
 				
 				//$("#api-text ul").text("");
 
@@ -50,9 +83,14 @@ $(document).ready(function() {
 
 		socket.emit("chat message", messageObject); //the function is to emit a command called chat message (we get to name this command "chat message") and along with that command attach some information to it (the value)//"chat message is let the server know that it's going to be a message"
 		$("#chat-input").val(""); //empties the box when you hit enter
+		$("#sendbutton").css("background-color", "rgb("+buttoncolorchange+",200,180)");
+		$("#chat-name").css("color", "rgb("+buttoncolorchange+",200,180)");
+		$("#chat-start").css("color", "rgb("+buttoncolorchange+",200,180)");
+		$("#chat-log ul li.them-line").css("background-color", "rgb("+buttoncolorchange+",200,180)");
+		$("#chat-input").css("color", "rgb("+buttoncolorchange+",200,180)");
+		$("#chat-input").css("border-color", "rgb("+buttoncolorchange+",200,180)");
 		return false; //prevents from refreshing page
-			
-		//$("#sendbutton").css("background-color", "rgb("+numberofwords+",200,180)");	
+				
 
 		});
 
@@ -88,14 +126,13 @@ $(document).ready(function() {
 
 		
 
-
-
 	socket.on('chat message', function(msg){ //when we get an event called chat message, run the following function w/ parameter called msg and append to our list that message
+
 		if(msg.username == $("#chat-name").val())
 			{
-				$("#chat-log ul").append("<li class='me-line'><b>" + msg.username +  "</b>: <i>" + msg.text + "</i></li>"); //use ul because it's in the ul
+				$("#chat-log ul").append("<li class='me-line'><b>" + msg.username +  "</b>: <i>" + msg.text + " (" + time + ")</i></li>"); //use ul because it's in the ul
 			}else{
-				$("#chat-log ul").append("<li class='them-line'><b>" + msg.username +  "</b>: <i>" + msg.text + "</i></li>");
+				$("#chat-log ul").append("<li class='them-line'><b>" + msg.username +  "</b>: <i>" + msg.text + " (" + time + ")</i></li>");
 
 			}	
 
